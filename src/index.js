a776e8ff069dbb4432e033b86d10a2101aabc6cd
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-17 20:22:26 +1000

    update

--- src/index.js
+++ src/index.js
@@ -4,13 +4,24 @@ const app = Vue.createApp({
   template: `
     <p>{{ count }}</p>
     <button v-on:click="increment">Increment</button>
+
+    <p v-if="isEven()">
+      Even
+    </p>
+    <p v-else>
+      Odd
+    </p>
   `,
   data() {
     return {
-      count: 0
+      count: 0,
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     }
   },
   methods: {
+    isEven () {
+      return this.count % 2 === 0
+    },
     increment() {
       this.count += 1
     }
