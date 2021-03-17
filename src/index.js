27fb3c4d7bb936e8706a0271f6d248efc37dc47c
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-17 21:42:48 +1000

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import * as Vue from 'vue/dist/vue.esm-bundler.js'
 
+// username
+// 5, 10
+
 const app = Vue.createApp({
   template: `
     <p>{{ count }}</p>
     <button @click="add">Increment</button>
-
     <ul>
       <li 
         v-for="number in numbers"
@@ -14,19 +16,30 @@ const app = Vue.createApp({
         {{ number }}
       </li>
     </ul>
+    <input @input="input" :value="username" />
+    <div v-if="error">{{ error }}</div>
   `,
   data() {
     return {
       count: 0,
-      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      username: 'user',
     }
   },
   computed: {
     evenNumbers() {
       return this.numbers.filter(num => this.isEven(num))
+    },
+    error() {
+      if (this.username.length < 5) {
+        return 'Too short'
+      } 
     }
   },
   methods: {
+    input($event) {
+      this.username = $event.target.value
+    },
     getClass(number) {
       return this.isEven(number) ? 'red' : 'blue'
     },
