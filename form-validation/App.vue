d5cd75df524ed9d5a9427c8cb9b1ba6742c476d6
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-24 18:59:21 +1000

    update

--- form-validation/App.vue
+++ form-validation/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>Hello</div> 
+  <my-button 
+    color="white"
+    background="darkslateblue"
+  />
 </template>
 
 <script>
+import MyButton from './MyButton.vue'
+
 export default {
+  components: {
+    MyButton
+  }
 }
 </script>
 
---
+++ form-validation/MyButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button v-bind:style="{ 
+    background,
+    color,
+  }">My Button</button>
+</template>
+
+<script>
+export default {
+  props: {
+    background: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  padding: 10px 40px;
+  border-radius: 8px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+button:hover {
+  filter: brightness(125%);
+}
+</style>
