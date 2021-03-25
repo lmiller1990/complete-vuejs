d13c0a08526e2a544e3165082a074b7c203c4f90
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-25 18:36:17 +1000

    update card

--- pokemon/App.vue
+++ pokemon/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div class="cards">
-    <div class="card" v-for="p in pokemon" :key="p.id">
-      <div class="title">
+    <card v-for="p in pokemon" :key="p.id" :p="p">
+      <template v-slot:title>
         {{ p.name }}
-      </div>
+      </template>
 
-      <div class="content">
+      <template v-slot:content>
         <img :src="p.sprite" />
-      </div>
+      </template>
 
-      <div class="description">
+      <template v-slot:description>
         <div v-for="type in p.types" :key="type.name">
           {{ type.name }}
         </div>
-      </div>
-    </div>
+      </template>
+    </card>
   </div>
 </template>
 
 <script>
+import Card from './Card.vue'
+
 const api = 'https://pokeapi.co/api/v2/pokemon'
 const ids = [1, 4, 7]
 
 export default {
+  components: {
+    Card
+  },
+
   data() {
     return {
       pokemon: []
@@ -59,36 +65,11 @@ export default {
 </script>
 
 <style scoped>
-.cards {
-  display: flex;
-}
-
 img {
   width: 100%;
 }
 
-.card {
-  border: 1px solid silver;
-  border-radius: 8px;
-  max-width: 200px;
-  margin: 0 5px;
-  cursor: pointer;
-  box-shadow: 0px 1px 3px darkgrey;
-  transition: 0.2s;
-}
-.title, .content, .description {
-  padding: 16px;
-  text-transform: capitalize;
-  text-align: center;
-}
-.title, .content {
-  border-bottom: 1px solid silver;
-}
-.title {
-  font-size: 1.25em;
-}
-.card:hover {
-  transition: 0.2s;
-  box-shadow: 0px 1px 9px darkgrey;
+.cards {
+  display: flex;
 }
 </style>
---
+++ pokemon/Card.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card">
+    <div class="title">
+      <slot name="title" />
+    </div>
+
+    <div class="content">
+      <slot name="content" />
+    </div>
+
+    <div class="description">
+      <slot name="description" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid silver;
+  border-radius: 8px;
+  max-width: 200px;
+  margin: 0 5px;
+  cursor: pointer;
+  box-shadow: 0px 1px 3px darkgrey;
+  transition: 0.2s;
+}
+.title, .content, .description {
+  padding: 16px;
+  text-transform: capitalize;
+  text-align: center;
+}
+.title, .content {
+  border-bottom: 1px solid silver;
+}
+.title {
+  font-size: 1.25em;
+}
+.card:hover {
+  transition: 0.2s;
+  box-shadow: 0px 1px 9px darkgrey;
+}
+</style>
