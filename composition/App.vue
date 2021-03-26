a531d789d0085359ad6d31a70385d71c44b3b298
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-26 19:26:36 +1000

    update

--- composition/App.vue
+++ composition/App.vue
@@ -1,13 +1,30 @@
 <template>
-  Hello {{ msg }}
+  <button @click="increment">{{ count }}</button>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   setup() {
+    const count = ref(0)
+
+    const increment = () => {
+      console.log(count.value)
+      count.value++
+    }
+
     return {
-      msg: 'world'
+      msg: 'world',
+      count,
+      increment
     }
   }
 }
 </script>
+
+<style scoped>
+button {
+  font-size: 40px;
+}
+</style>
