c9a4152ce783a73e8265071f1bb7fbe7bbe782ac
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-26 19:43:33 +1000

    update

--- composition/App.vue
+++ composition/App.vue
@@ -2,14 +2,18 @@
   <button @click="increment">{{ count }}</button>
   <button @click="increase('a')">{{ numbers.a }}</button>
   <button @click="increase('b')">{{ numbers.b }}</button>
+  <h1>{{ double }}</h1>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { ref, reactive, computed } from 'vue'
 
 export default {
   setup() {
+    // primitive
     const count = ref(0)
+
+    // object
     const numbers = reactive({
       a: 0,
       b: 0
@@ -23,7 +27,17 @@ export default {
       count.value++
     }
 
+    const total = computed(() => {
+      return count.value + numbers.a + numbers.b
+    })
+
+    const double = computed(() => {
+      return total.value * 2
+    })
+
     return {
+      total,
+      double,
       msg: 'world',
       count,
       increment,
