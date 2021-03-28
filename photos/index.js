d8856eb523432b2d9cde19e9b39bde5a92de42d4
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-28 20:17:13 +1000

    init

--- photos/index.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { store } from './store.js'
-import { router } from './router.js'
-
-const app = createApp(App)
-app.use(store)
-app.use(router)
-app.mount('#app')
--- vuex/index.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import { store } from './store.js'
-import AppOptions from './AppOptions.vue'
-
-const app = createApp(AppOptions)
-app.use(store)
-app.mount('#app')
