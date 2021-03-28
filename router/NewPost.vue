034eb16fd113f2710d53305723f12bb386a87a1f
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-28 14:43:09 +1000

    Update

--- router/NewPost.vue
+++ router/NewPost.vue
@@ -8,17 +8,22 @@
 <script>
 import { ref } from 'vue'
 import { usePosts } from './usePosts.js'
+import { useRouter } from 'vue-router'
 
 export default {
   setup() {
+    const router = useRouter()
     const posts = usePosts()
     const content = ref('')
+
     const submit = () => {
+      const id = posts.posts.value.length + 1
       posts.createPost({
-        id: posts.posts.value.length + 1,
+        id,
         title: 'New Post',
         content: content.value,
       })
+      router.push(`/posts/${id}`)
     }
 
     return {
