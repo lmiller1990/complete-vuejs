36b2d6ef568b72d8fe1a63ab0fba4ffcaa2f6797
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-28 17:03:12 +1000

    update

--- photos/App.vue
+++ photos/App.vue
@@ -1,11 +1,28 @@
 <template>
-  Photos
+  <layout>
+    <template v-slot:header>
+      Header
+    </template>
+
+    <template v-slot:sidebar>
+      Sidebar
+    </template>
+
+    <template v-slot:content>
+      Content
+    </template>
+  </layout>
 </template>
 
 <script>
 import { useStore } from 'vuex'
+import Layout from './Layout.vue'
 
 export default {
+  components: {
+    Layout,
+  },
+
   setup() {
     const store = useStore()
 
---
+++ photos/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper">
+    <div class="header">
+      <slot name="header" />
+    </div>
+
+    <div class="main">
+      <slot name="sidebar" />
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
