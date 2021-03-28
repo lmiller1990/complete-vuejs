4d6fd5ad03496956229a8bdc76afcb3e0698182a
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-03-28 15:16:27 +1000

    init

---
+++ vuex/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ store.state.count }}</div>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+
+export default {
+  setup() {
+    const store = useStore()
+
+    return {
+      store
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-size: 40px;
+}
+</style>
